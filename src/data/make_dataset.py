0392192832b0b9250f534c20449c1e1c192df890
Saiha Mohammed <saiha0911@gmail.com>
2020-04-06 14:53:04 -0500

    updated the load train function to pass clean data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -50,11 +50,15 @@ def create_clean_train_data():
     y_train.to_csv(Y_TRAIN_CLEAN_PATH,index=False)
 
 
-def load_training_data():
+def load_training_data(clean=False):
     ''' return X_train and y_train if they exist.
     '''
-    X_train=pd.read_csv(X_TRAIN_RAW_PATH)
-    y_train=pd.read_csv(Y_TRAIN_RAW_PATH)
+    if clean:
+        X_train=pd.read_csv(X_TRAIN_CLEAN_PATH)
+        y_train=pd.read_csv(Y_TRAIN_CLEAN_PATH)
+    else:
+        X_train=pd.read_csv(X_TRAIN_RAW_PATH)
+        y_train=pd.read_csv(Y_TRAIN_RAW_PATH)
     
     return X_train,y_train 
 
