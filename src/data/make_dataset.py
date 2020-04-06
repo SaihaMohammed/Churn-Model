9ba4e510ad8756591961e57cc0861cd5d7716814
Saiha Mohammed <saiha0911@gmail.com>
2020-04-05 21:39:18 -0500

    added functions to read training and testing datasets

---
+++ src/data/make_dataset.py
@@ -0,0 +1,56 @@
+
+import os
+import sys
+sys.path.append(".")
+
+import click
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from src.localpath import *
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def create_train_test_split():
+    '''create X and y training set files and holdout set files in the data/raw directory'''
+    print('Loading the data')
+    df = pd.read_csv(RAW_DATA_PATH)
+
+    print('creating X and y')
+    X= df.drop(columns="Churn")
+    y= df[["Churn"]]
+
+    print('Creating train-test split')
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+
+    print("Saving data files")
+    X_train.to_csv(X_TRAIN_RAW_PATH,index=False)
+    X_test.to_csv(X_TEST_RAW_PATH,index=False)
+    y_train.to_csv(Y_TRAIN_RAW_PATH,index=False)
+    y_test.to_csv(Y_TEST_RAW_PATH,index=False)
+
+def load_training_data():
+    ''' return X_train and y_train if they exist.
+    '''
+    X_train=pd.read_csv(X_TRAIN_RAW_PATH)
+    y_train=pd.read_csv(Y_TRAIN_RAW_PATH)
+    
+    return X_train,y_train 
+
+def load_testing_data():
+    ''' return X_train and y_train if they exist.
+    '''
+    X_test=pd.read_csv(X_TEST_RAW_PATH)
+    y_test=pd.read_csv(Y_TEST_RAW_PATH)
+    
+    return X_test,y_test
+
+
+
+if __name__=="__main__":
+    cli()
+
