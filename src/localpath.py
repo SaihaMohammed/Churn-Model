2b62861d2bd85030772ccf61e186b99edea30e33
Saiha Mohammed <saiha0911@gmail.com>
2020-04-06 20:23:14 -0500

    added path to save featurized data

--- src/localpath.py
+++ src/localpath.py
@@ -5,7 +5,7 @@ ROOT_DIRECTORY=os.path.split(SRC_DIRECTORY)[0]
 DATA_DIRECTORY=os.path.join(ROOT_DIRECTORY,'data')
 INTERIM_DATA_DIRECTORY=os.path.join(DATA_DIRECTORY,'interim')
 RAW_DATA_DIRECTORY=os.path.join(DATA_DIRECTORY,'raw')
-PROCESSED_DATA_DIRECTORY=os.path.join(DATA_DIRECTORY,'data')
+PROCESSED_DATA_DIRECTORY=os.path.join(DATA_DIRECTORY,'processed')
 
 #File paths - Raw data
 RAW_DATA_PATH= os.path.join(RAW_DATA_DIRECTORY,"WA_Fn-UseC_-Telco-Customer-Churn.csv")
@@ -20,3 +20,7 @@ Y_TEST_RAW_PATH=os.path.join(RAW_DATA_DIRECTORY,"y_test.csv")
 #File paths cleaned train data
 X_TRAIN_CLEAN_PATH =os.path.join(INTERIM_DATA_DIRECTORY,'X_train.csv')
 Y_TRAIN_CLEAN_PATH = os.path.join(INTERIM_DATA_DIRECTORY,'y_train.csv')
+
+#File paths Featurized train-test split data ready fro modelling
+X_TRAIN_FEATURIZED_PATH =os.path.join(PROCESSED_DATA_DIRECTORY,'X_train.csv')
+Y_TRAIN_FEATURIZED_PATH = os.path.join(PROCESSED_DATA_DIRECTORY,'y_train.csv')
