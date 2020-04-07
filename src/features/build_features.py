c119adacd7118c04cc44cfa2e2e33eb82089af78
Saiha Mohammed <saiha0911@gmail.com>
2020-04-06 21:29:08 -0500

    converted binary values into 0 ad 1s

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -23,6 +23,7 @@ def create_featurized_data():
 
     print('featurizing data')
     X_train= drop_customer_id(X_train)
+    X_train= transform_binary_categorical(X_train)
 
     print("Saving data")
     X_train.to_csv(X_TRAIN_FEATURIZED_PATH,index=False)
@@ -35,6 +36,17 @@ def drop_customer_id(X_train):
     '''
     X_train = X_train.drop(columns=['customerID'])
     return X_train
+
+def transform_binary_categorical(X_train):
+    '''transforms binary categorical features into 
+    1 ans 0s'''
+    X_train['gender']=X_train['gender'].map({'Female':1,'Male':0})
+    X_train['Partner']=X_train['Partner'].map({'Yes':1,'No':0})
+    X_train['Dependents']=X_train['Dependents'].map({'Yes':1,'No':0})
+    X_train['PhoneService']=X_train['PhoneService'].map({'Yes':1,'No':0})
+    X_train['PaperlessBilling']=X_train['PaperlessBilling'].map({'Yes':1,'No':0})
+    return X_train
+    
 
 
 
